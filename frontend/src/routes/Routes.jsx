b2e27ef7c1be647e125cad52f8636554e19118ce
Niloy <nafis.sadique.niloy@g.bracu.ac.bd>
2024-12-20 18:28:22 +0600

    implement login page: add Login component with form handling, club selection, and authentication logic

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layouts/Root";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
+      {
+        path: '/login',
+        element: <Login/>,
+      }
     ],
   },
 ]);
